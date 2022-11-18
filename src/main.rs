b60395c9b9ea3e85cfe3a50cea2c72b58e3440fb
ChenYing Kuo <evshary@gmail.com>
2022-11-18 13:47:11 +0800

    Add crossterm and able to control the turtle with keys

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use zenoh::prelude::r#async::*;
 use serde_derive::Serialize;
 use cdr::{CdrLe, Infinite};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 
 #[derive(Serialize, PartialEq)]
 struct Vector3 {
@@ -39,5 +40,37 @@ async fn main() {
     println!("Opening session...");
     let session = zenoh::open(Config::default()).res().await.unwrap();
     let key = String::from("rt/turtle1/cmd_vel");
-    pub_twist(&session, &key, 1.0, 1.0).await;
+    println!("Press arrow keys to control turtle...");
+    crossterm::terminal::enable_raw_mode().unwrap();
+    loop {
+        match crossterm::event::read() {
+            Ok(Event::Key(KeyEvent { code: KeyCode::Up, modifiers: _, kind: _, state: _ })) => {
+                pub_twist(&session, &key, 2.0, 0.0).await
+            },
+            Ok(Event::Key(KeyEvent { code: KeyCode::Down, modifiers: _, kind: _, state: _ })) => {
+                pub_twist(&session, &key, -2.0, 0.0).await
+            },
+            Ok(Event::Key(KeyEvent { code: KeyCode::Left, modifiers: _, kind: _, state: _ })) => {
+                pub_twist(&session, &key, 0.0, 2.0).await
+            },
+            Ok(Event::Key(KeyEvent { code: KeyCode::Right, modifiers: _, kind: _, state: _ })) => {
+                pub_twist(&session, &key, 0.0, -2.0).await
+            },
+            Ok(Event::Key(KeyEvent { code: KeyCode::Char(' '), modifiers: _, kind: _, state: _ })) => {
+                pub_twist(&session, &key, 0.0, 0.0).await
+            },
+            Ok(Event::Key(KeyEvent { code: KeyCode::Esc, modifiers: _, kind: _, state: _ })) |
+            Ok(Event::Key(KeyEvent { code: KeyCode::Char('q'), modifiers: _, kind: _, state: _ })) => {
+                break
+            },
+            Ok(Event::Key(KeyEvent { code: KeyCode::Char('c'), modifiers, kind: _, state: _})) => {
+                if modifiers.contains(KeyModifiers::CONTROL) { break }
+            },
+            Ok(_) => (),
+            Err(_) => {}
+        }
+    }
+    // Stop the robot
+    pub_twist(&session, &key, 0.0, 0.0).await;
+    crossterm::terminal::disable_raw_mode().unwrap();
 }
